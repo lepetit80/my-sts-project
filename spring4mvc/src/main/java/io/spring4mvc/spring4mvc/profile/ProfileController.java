d6a48c44457a2b723f310b7917f77c79e742205d
terrybyte <terrybyte@naver.com>
2017-07-22 20:47:29 +0900

    localDate ko

--- spring4mvc/src/main/java/io/spring4mvc/spring4mvc/profile/ProfileController.java
+++ spring4mvc/src/main/java/io/spring4mvc/spring4mvc/profile/ProfileController.java
@@ -2,24 +2,34 @@ package io.spring4mvc.spring4mvc.profile;
 
 import java.util.Locale;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import io.spring4mvc.spring4mvc.date.KRLocalDateFormatter;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class ProfileController {
 	
 	@GetMapping("/profile")
 	public String displayProfile(ProfileForm profileForm) {
+		//profileForm.setEmail("default@email.com");
 		return "profile/profilePage";
 	}
 	
 	@PostMapping("/profile")
-	public String saveProfile(ProfileForm profileForm) {
-		System.out.println("saved: " + profileForm.toString());
+	public String saveProfile(@Valid ProfileForm profileForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "profile/profilePage";
+		}
+		
+		log.debug("ProfileForm: {}", profileForm);
 		return "redirect:/profile";
 	}
 	
--- spring4mvc/src/main/java/io/spring4mvc/spring4mvc/profile/ProfileForm.java
+++ spring4mvc/src/main/java/io/spring4mvc/spring4mvc/profile/ProfileForm.java
@@ -4,12 +4,29 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import io.spring4mvc.spring4mvc.date.PastLocalDate;
 import lombok.Data;
 
 @Data
 public class ProfileForm {
+	
+	@Size(min = 2)
     private String twitterHandle;
+	
+	@Email
+	@NotEmpty
     private String email;
+	
+	@NotNull
+	@PastLocalDate
     private LocalDate birthDate;
+	
+	@NotEmpty
     private List<String> tastes = new ArrayList<>();
 }
