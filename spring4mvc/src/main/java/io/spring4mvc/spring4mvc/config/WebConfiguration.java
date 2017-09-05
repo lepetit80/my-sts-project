393b4149f36f975b118ac6ecf6eb0a209f2337ad
lepetit80 <lepetit80@gmail.com>
2017-09-05 21:50:19 +0900

    4.1.4 containerCustomizer issue

--- spring4mvc/src/main/java/io/spring4mvc/spring4mvc/config/WebConfiguration.java
+++ spring4mvc/src/main/java/io/spring4mvc/spring4mvc/config/WebConfiguration.java
@@ -2,10 +2,13 @@ package io.spring4mvc.spring4mvc.config;
 
 import java.time.LocalDate;
 
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ErrorPage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -38,5 +41,20 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+//		EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer = new EmbeddedServletContainerCustomizer() {
+//			@Override
+//			public void customize(ConfigurableEmbeddedServletContainer container) {
+//				container.addErrorPages(new ErrorPage(MultipartException.class, "/upload-error"));
+//			}
+//		};
+		
+		EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer = 
+			container -> container.addErrorPages(new ErrorPage(MultipartException.class, "/upload-error"));
+			
+		return embeddedServletContainerCustomizer;
 	}
 }
--- spring4mvc/src/main/java/io/spring4mvc/spring4mvc/profile/PictureUploadController.java
+++ spring4mvc/src/main/java/io/spring4mvc/spring4mvc/profile/PictureUploadController.java
@@ -1,29 +1,30 @@
 package io.spring4mvc.spring4mvc.profile;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URLConnection;
-import java.nio.file.Files;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.WebUtils;
 
 import io.spring4mvc.spring4mvc.config.PicturesUploadProperties;
 
@@ -33,6 +34,21 @@ public class PictureUploadController {
 
 	private final Resource pictureDir;
 	private final Resource anonymousPicture;
+	
+	//TODO - #1-issue not working why?
+	@RequestMapping("/upload-error")
+	public ModelAndView onUploadError(HttpServletRequest request) {
+		ModelAndView mav = new ModelAndView("profile/uploadPage");
+		mav.addObject("error", request.getAttribute(WebUtils.ERROR_EXCEPTION_ATTRIBUTE));
+		return mav;
+	}
+	
+	@ExceptionHandler(IOException.class)
+	public ModelAndView handleIOException(IOException exception) {
+		ModelAndView mav = new ModelAndView("profile/uploadPage");
+		mav.addObject("error", exception.getMessage());
+		return mav;
+	}
 	
 	@Autowired
 	public PictureUploadController(PicturesUploadProperties picturesUploadProperties) {
@@ -66,8 +82,9 @@ public class PictureUploadController {
 	public String onUpload(MultipartFile file, RedirectAttributes redirectAttributes, Model model) throws IOException {
 		
 		if (file.isEmpty() || !isImage(file)) {
-			redirectAttributes.addFlashAttribute("error", "Incorrect file. plz upload a picture.");
-			return "redirect:/upload";
+//			redirectAttributes.addFlashAttribute("error", "Incorrect file. plz upload a picture.");
+//			return "redirect:/upload";
+			throw new IOException("Incorrect file. plz upload a picture.");
 		}
 		
 		Resource picturePath = copyFileToPictures(file);
